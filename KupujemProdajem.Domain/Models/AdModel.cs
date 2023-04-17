99b7945551cd6e53c84e985579ed809a6ac3671d
TheStankela <ognjenogistankovic2002@gmail.com>
2023-04-17 21:35:09 +0200

    Updated model for photo service

--- KupujemProdajem.Domain/Models/AdModel.cs
+++ KupujemProdajem.Domain/Models/AdModel.cs
@@ -18,5 +18,6 @@ namespace KupujemProdajem.Domain.Models
         public UserModel? User { get; set; }
         public int CategoryId { get; set; }
         public CategoryModel Category { get; set; }
+        //public string? ImageUrl { get; set; }
     }
 }
